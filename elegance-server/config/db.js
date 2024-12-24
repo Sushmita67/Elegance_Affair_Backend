d35eeff8c3865e2761ce7f2cc33eb6a754d28d7c
Sushmita67 <biswakarmasushmita5@gmail.com>
2024-12-24 15:08:31 +0545

    Update db.js

--- elegance-server/config/db.js
+++ elegance-server/config/db.js
@@ -3,7 +3,6 @@ const mongoose=require("mongoose")
 const connectDB=async()=>{
     try{
         await mongoose.connect("mongodb://localhost:27017/elegance_db");
-        //wait husssss
         console.log("Database connection established successfully!");
     }
     catch(e){
